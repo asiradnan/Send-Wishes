022bdb84fd4228d9d4386c352f6615381c5dcc32
Asir <asiradnan23@gmail.com>
2024-05-27 14:06:44 +0600

    Added core functionalities

--- ReceiveEmail/views.py
+++ ReceiveEmail/views.py
@@ -1,4 +1,4 @@
-from django.core .mail import send_mail
+from django.core.mail import send_mail,get_connection,EmailMessage
 from django.shortcuts import render 
 from . import settings
 from django.http import HttpResponse
@@ -6,10 +6,20 @@ def home(request):
     if request.method=="POST":
         subject = request.POST['subject']
         body = request.POST['body']
-        email = request.POST['email']
-        x = send_mail(subject,body,settings.EMAIL_HOST,[email])
-        if (x==1):
-            return HttpResponse("Email sent successfully!")
-        return HttpResponse("Error")
+        to_list = request.POST['to'].split(',')
+        bcc_list = request.POST['bcc'].split(',')
+        file = request.FILES.get('file',None)
+        if len(to_list)>0 and len(bcc_list)>0:
+            email = EmailMessage(subject,body,settings.EMAIL_HOST_USER,to_list,bcc_list)
+        elif len(to_list)>0:
+            email = EmailMessage(subject,body,settings.EMAIL_HOST_USER,to_list)
+        elif len(bcc_list)>0:
+            email = EmailMessage(subject,body,settings.EMAIL_HOST_USER,[],bcc_list)
+        if file is not None:
+            email.attach(file.name, file.read(), file.content_type)
+        x= email.send()
+        if x==0:
+            return HttpResponse("Error occured")
+        return HttpResponse("Sent successfully!")
     return render(request,"home.html")
     
